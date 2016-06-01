960dd14b58088fb266e88fc920aafe3830c7557c
Deepak Grover <deepakgrover333@gmail.com>
2016-06-01 23:05:55 +0530

    update django admin settings

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fields = ['pub_date', 'question_text']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
